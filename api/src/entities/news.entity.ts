0dcd44bafaa5b69babc8a5e2a74619a00e38b9c4
Chloengr <chloe.nger@gmail.com>
2022-01-24 16:08:03 +0100

    :sparkles: add news entity + GET

--- api/src/entities/news.entity.ts
+++ api/src/entities/news.entity.ts
@@ -1,26 +1,42 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
-import { Exclude, Expose } from 'class-transformer';
+import { DateType, Entity, Enum, PrimaryKey, Property, Unique } from '@mikro-orm/core';
 import { v4 } from 'uuid';
+
+export enum NewsType {
+  None = "none",
+  GoodPractice = "goodPractice",
+  Announcement = "announcement",
+  Info = "info",
+  Event = "event",
+  Warning = "warning"
+}
 
 @Entity()
 export class NewsEntity {
+
   @PrimaryKey({ type: 'uuid' })
+  @Unique()
   uuid: string = v4();
 
-  @Property()
-  content: string;
+  @Enum({ items: () => NewsType})
+  type: NewsType;
 
-  @Exclude()
+  @Property({ length: 200 })
+  message: string;
+
+  @Property({ type: DateType })
+  startDate: Date;
+
+  @Property({ type: DateType, nullable: true })
+  endDate?: Date;
+
   @Property({ nullable: true })
-  test?: string | undefined;
+  link?: string;
 
-  @Expose()
-  get calculatedValue(): string {
-    return `It is a calculated value, not in BD yeeaah`;
-  }
+  @Property({ nullable: true })
+  linkTitle?: string;
 
-  @Expose()
-  valueNotInBd?: string;
+  @Property()
+  displayPeriod: boolean;
 
   constructor(partial: Partial<NewsEntity>) {
     Object.assign(this, partial);
