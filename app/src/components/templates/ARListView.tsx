5f300aa147d72727a7a597cd0ef9af961a643b99
Chloengr <chloe.nger@gmail.com>
2022-02-14 15:56:16 +0100

    list view screen

---
+++ app/src/components/templates/ARListView.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const styles = StyleSheet.create({})
+
+const ARListView = () => {
+  return (
+    <View>
+      <Text>ARListView</Text>
+    </View>
+  )
+}
+
+export default ARListView
