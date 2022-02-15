d26139c0211df57dcf6690443936536ca17e41f3
Johbrun <31695713+Johbrun@users.noreply.github.com>
2022-02-15 11:25:58 +0100

    :wrench: config fk_check for migrations

--- api/src/mikro-orm.config.ts
+++ api/src/mikro-orm.config.ts
@@ -11,8 +11,13 @@ const config: MikroOrmModuleSyncOptions = {
   entitiesTs: ['./src/entities'],
   type: 'postgresql',
   metadataProvider: TsMorphMetadataProvider,
-  dbName: process.env.ORM_DB ,
+  dbName: process.env.ORM_DB,
   clientUrl: process.env.ORM_URL,
+  migrations: {
+    path: './migrations', // path to the folder with migrations
+    transactional: true, // wrap each migration in a transaction
+    disableForeignKeys: false, // wrap statements with `set foreign_key_checks = 0` or equivalent
+  },
 };
 
 export default config;
