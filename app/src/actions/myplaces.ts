a0b8a827f4f63824da4cb220fccb54b6673565ef
Chloengr <chloe.nger@gmail.com>
2022-02-23 18:38:59 +0100

    :sparkles: create my address

---
+++ app/src/actions/myplaces.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { POI } from './poi';
+
+export const getAllPlaces = async () => {
+  try {
+    const keys = await AsyncStorage.getAllKeys();
+    const result = await AsyncStorage.multiGet(keys as string[]);
+    //TODO check myplace_ prefix to render wanted item
+    return result!.map(tab => JSON.parse(tab[1]!));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getPlaceById = async (key: string) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const setPlaceStorage = async (place: POI) => {
+  try {
+    const jsonValue = JSON.stringify(place);
+    await AsyncStorage.setItem(`myplace_${place?.id}`, jsonValue);
+  } catch (e) {
+    console.error(e)
+  }
+};
