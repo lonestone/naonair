48a53bbe23765e2ec0c8358d4bfce4b8eb9e38a7
Julien Smolareck <julien.smolareck@lonestone.studio>
2022-02-28 12:09:04 +0100

    ✨ Add sentry

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,13 +10,21 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'react-native-paper';
 import Screens from './screens/NavigatorScreen';
 import { theme } from './theme';
+import { SENTRY } from './config.json';
+
+if (!__DEV__) {
+  Sentry.init({
+    dsn: SENTRY.dsn,
+  });
+}
 
 const App = () => {
-  return (
+  const jsx = (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle={'dark-content'} />
       <NavigationContainer>
@@ -24,6 +32,12 @@ const App = () => {
       </NavigationContainer>
     </ThemeProvider>
   );
+
+  if (__DEV__) {
+    return jsx;
+  }
+
+  return Sentry.wrap(() => jsx);
 };
 
 export default App;
