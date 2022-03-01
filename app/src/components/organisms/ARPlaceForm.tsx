ebb9b9b1473e2b6cd670f28fe2745d6cf2bd9e91
Chloengr <chloe.nger@gmail.com>
2022-03-01 12:28:26 +0100

    :sparkles: edit a place
    # Conflicts:
    #	app/src/Screens/NavigatorScreen.tsx
    #	app/src/types/routes.tsx

---
+++ app/src/components/organisms/ARPlaceForm.tsx
@@ -0,0 +1,70 @@
+import { Position } from 'geojson';
+import React, { Dispatch, SetStateAction } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { MapboxFeature } from '../../actions/poi';
+import { theme } from '../../theme';
+import ARAddressInput from '../molecules/ARAddressInput';
+
+const styles = StyleSheet.create({
+  input: {
+    marginHorizontal: 15,
+    marginVertical: 5,
+    color: theme.colors.blue[400],
+    backgroundColor: 'white',
+  },
+});
+
+interface ARPlaceFormProps {
+  name: string;
+  setName: Dispatch<SetStateAction<string>>;
+  values?: {
+    coord: Position;
+    text: string;
+  };
+  setValues: Dispatch<
+    SetStateAction<
+      | {
+          coord: Position;
+          text: string;
+        }
+      | undefined
+    >
+  >;
+  setResults: Dispatch<SetStateAction<MapboxFeature[]>>;
+}
+
+const ARPlaceForm = ({
+  name,
+  setName,
+  values,
+  setValues,
+  setResults,
+}: ARPlaceFormProps) => {
+  return (
+    <View>
+      <TextInput
+        label="Titre"
+        placeholder="Titre"
+        value={name}
+        mode="outlined"
+        onChangeText={text => setName(text)}
+        style={styles.input}
+        outlineColor={theme.colors.blue[400]}
+        placeholderTextColor={theme.colors.blue[400]}
+      />
+      <ARAddressInput
+        label="Adresse"
+        placeholder="Adresse"
+        onResults={setResults}
+        onUserLocation={(coord, text) => {
+          setValues({ coord, text });
+        }}
+        value={values}
+        style={styles.input}
+      />
+    </View>
+  );
+};
+
+export default ARPlaceForm;
--- app/src/components/templates/ARPlaceFormLayout.tsx
+++ app/src/components/templates/ARPlaceFormLayout.tsx
@@ -1,27 +1,20 @@
-import { useNavigation } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { Position } from 'geojson';
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
-import { setPlaceStorage } from '../../actions/myplaces';
-import { MapboxFeature, POI, POICategory } from '../../actions/poi';
+import { setPlaceStorage, updatePlaceStorage } from '../../actions/myplaces';
+import { MapboxFeature, POICategory } from '../../actions/poi';
 import useSnackbar from '../../contexts/snackbar.context';
 import { theme } from '../../theme';
-import { StackNavigationScreenProp } from '../../types/routes';
+import { StackNavigationScreenProp, StackParamList } from '../../types/routes';
 import { ARButton, ARButtonSize } from '../atoms/ARButton';
-import ARAddressInput from '../molecules/ARAddressInput';
 import ARListItem from '../molecules/ARListItem';
+import ARPlaceForm from '../organisms/ARPlaceForm';
 import { icons } from './ARMapView';
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: 'white' },
-  input: {
-    marginHorizontal: 15,
-    marginVertical: 5,
-    color: theme.colors.blue[400],
-    backgroundColor: 'white',
-  },
   button: {
     margin: 40,
     flex: 0,
@@ -31,8 +24,11 @@ const styles = StyleSheet.create({
   },
 });
 
-const ARCreatePlace = () => {
+type ARPlaceFormType = RouteProp<StackParamList, 'PlaceForm'>;
+
+const ARPlaceFormLayout = () => {
   const navigation = useNavigation<StackNavigationScreenProp>();
+  const { params } = useRoute<ARPlaceFormType>();
 
   const { setSnackbarStatus } = useSnackbar();
 
@@ -43,12 +39,18 @@ const ARCreatePlace = () => {
   >();
   const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    if (!params) return;
+    setName(params.poi.name);
+    setValues({ coord: params.poi.geolocation, text: params.poi.adress });
+  }, []);
+
   const handleOnsubmit = async () => {
     if (values) {
       setIsLoading(true);
 
       await setPlaceStorage({
-        id: new Date().getTime(),
+        id: Date.now(),
         adress: values?.text,
         category: POICategory.MY_PLACES,
         name,
@@ -65,29 +67,46 @@ const ARCreatePlace = () => {
       });
       setIsLoading(false);
     }
-  };  
+  };
+
+  const handleOnUpdatesubmit = async () => {
+    if (params && values) {
+      setIsLoading(true);
+
+      await updatePlaceStorage(params.poi.id, {
+        id: Date.now(),
+        adress: values.text,
+        category: POICategory.MY_PLACES,
+        name,
+        geolocation: values.coord,
+      });
+
+      navigation.goBack();
+
+      setSnackbarStatus?.({
+        isVisible: true,
+        label: "L'adresse a été bien été modifiée",
+        icon: 'check-circle',
+        backgroundColor: theme.colors.quality.main.green,
+      });
+      setIsLoading(false);
+    }
+  };
+
+  const isDisabled = useMemo(() => {
+    if (!values || values?.coord.length === 0 || !name.length) {
+      return true;
+    }
+  }, [name, values]);
 
   return (
     <View style={styles.container}>
-      <TextInput
-        label="Titre"
-        placeholder="Titre"
-        value={name}
-        mode="outlined"
-        onChangeText={text => setName(text)}
-        style={styles.input}
-        outlineColor={theme.colors.blue[400]}
-        placeholderTextColor={theme.colors.blue[400]}
-      />
-      <ARAddressInput
-        label="Adresse"
-        placeholder="Adresse"
-        onResults={setResults}
-        onUserLocation={(coord, text) => {
-          setValues({ coord, text });
-        }}
-        value={values}
-        style={styles.input}
+      <ARPlaceForm
+        name={name}
+        setName={setName}
+        values={values}
+        setValues={setValues}
+        setResults={setResults}
       />
       <ScrollView
         style={styles.container}
@@ -116,18 +135,27 @@ const ARCreatePlace = () => {
           </React.Fragment>
         ))}
       </ScrollView>
-      <ARButton
-        label="Enregistrer"
-        size={ARButtonSize.Medium}
-        styleContainer={styles.button}
-        onPress={handleOnsubmit}
-        disabled={
-          !values || values.coord.length === 0 || !name || name.length === 0
-        }
-        loading={isLoading}
-      />
+      {params ? (
+        <ARButton
+          label="Modifier"
+          size={ARButtonSize.Medium}
+          styleContainer={styles.button}
+          onPress={handleOnUpdatesubmit}
+          disabled={isDisabled}
+          loading={isLoading}
+        />
+      ) : (
+        <ARButton
+          label="Enregistrer"
+          size={ARButtonSize.Medium}
+          styleContainer={styles.button}
+          onPress={handleOnsubmit}
+          disabled={isDisabled}
+          loading={isLoading}
+        />
+      )}
     </View>
   );
 };
 
-export default ARCreatePlace;
+export default ARPlaceFormLayout;
