daf5ca502205257b452772421997fc98401110b4
Julien Smolareck <julien.smolareck@lonestone.studio>
2022-03-03 20:06:46 +0100

    ✨ Detect if the user disable phone's location and show a modal

---
+++ app/src/actions/location.ts
@@ -0,0 +1,37 @@
+import { PermissionsAndroid, Platform } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+export const checkAndroidPermission = async (): Promise<boolean> => {
+  try {
+    await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+    ]);
+  } catch (e) {
+    console.info(e);
+  }
+
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      () => {
+        resolve(true);
+      },
+      error => {
+        console.info('currentPosition', error, Platform.OS);
+        reject(false);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        showLocationDialog: true,
+        forceRequestLocation: true,
+      },
+    );
+  });
+};
+
+export const checkIOSPermission = async () => {
+  const result = await Geolocation.requestAuthorization('always');
+  return result === 'granted';
+};
--- app/src/components/templates/ARUserLocationAlert.tsx
+++ app/src/components/templates/ARUserLocationAlert.tsx
@@ -1,13 +1,10 @@
-import MapboxGL from '@react-native-mapbox-gl/maps';
-import Geolocation from 'react-native-geolocation-service';
-import React, { useCallback, useEffect, useState } from 'react';
-import {
-  Linking,
-  PermissionsAndroid,
-  Platform,
-  StyleSheet,
-} from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Linking, Platform, StyleSheet } from 'react-native';
 import { Button, Modal, Portal, Text } from 'react-native-paper';
+import {
+  checkAndroidPermission,
+  checkIOSPermission,
+} from '../../actions/location';
 
 const styles = StyleSheet.create({
   modal: {
@@ -21,46 +18,30 @@ export default () => {
   const [userLocationEnabled, setUserLocationEnabled] =
     useState<boolean>(false);
 
-  const checkAndroidPermission = useCallback(async () => {
+  const watchId = useRef<number | null>();
+
+  const watch = useCallback(async () => {
     try {
-      const granted = await PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-      ]);
-
-      Geolocation.getCurrentPosition(
-        () => setUserLocationEnabled(true),
-        error => console.info('currentPosition', error, Platform.OS),
-        {
-          enableHighAccuracy: true,
-          timeout: 15000,
-          maximumAge: 10000,
-          showLocationDialog: true,
-          forceRequestLocation: true,
-        },
-      );
+      if (Platform.OS === 'android') {
+        await checkAndroidPermission();
+      } else {
+        setUserLocationEnabled(await checkIOSPermission());
+      }
     } catch (e) {
-      console.info('checkAndroidPermission error:', { e });
+      console.info(e);
     }
-  }, []);
 
-  const checkIOSPermission = useCallback(async () => {
-    try {
-      const result = await Geolocation.requestAuthorization('always');
-
-      setUserLocationEnabled(result === 'granted');
-    } catch (e) {
-      console.info('checkIOSPermission error:', { e });
-    }
+    watchId.current = setTimeout(() => {
+      watch();
+    }, 1000) as unknown as number;
   }, []);
 
   useEffect(() => {
-    if (Platform.OS === 'android') {
-      checkAndroidPermission();
-    } else {
-      checkIOSPermission();
-    }
-  }, [checkAndroidPermission, checkIOSPermission]);
+    watch();
+    return () => {
+      watchId.current && clearTimeout(watchId.current);
+    };
+  }, [watch]);
 
   return Platform.OS === 'ios' ? (
     <Portal>
