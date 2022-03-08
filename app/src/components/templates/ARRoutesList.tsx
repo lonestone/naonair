1c081ca4e0713e767231f4e30f95d1f0a2ab269f
Julien Smolareck <julien.smolareck@lonestone.studio>
2022-03-08 15:03:11 +0100

    ✨  add filter to parcours list

--- app/src/components/templates/ARRoutesList.tsx
+++ app/src/components/templates/ARRoutesList.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View, VirtualizedList } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 import { ARParcours, getAll } from '../../actions/parcours';
+
 import ARRouteItem from '../molecules/ARRouteItem';
 
 const styles = StyleSheet.create({
@@ -16,22 +18,27 @@ const styles = StyleSheet.create({
   },
 });
 
-export interface ARRoutesListProps {}
+export interface ARRoutesListProps {
+  filters: string[];
+}
 
-export default () => {
+export default ({ filters }: ARRoutesListProps) => {
   const [parcours, setParcours] = useState<ARParcours[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getParcours = useCallback(async () => {
+    setIsLoading(true);
     try {
-      setParcours(await getAll());
+      setParcours(await getAll(filters));
     } catch (e) {
       console.info(e);
     }
-  }, []);
+    setIsLoading(false);
+  }, [filters]);
 
   useEffect(() => {
     getParcours();
-  }, [getParcours]);
+  }, [getParcours, filters]);
 
   return (
     <View style={styles.container}>
@@ -46,6 +53,9 @@ export default () => {
           getItemCount={data => data.length}
           getItem={(data, index) => data[index]}
         />
+      )}
+      {isLoading && (
+        <ActivityIndicator style={styles.container} size="large" animating />
       )}
     </View>
   );
