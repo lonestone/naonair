62f9f7f746538b87dde4ad2745884ac079db59ea
Julien Smolareck <julien.smolareck@lonestone.studio>
2022-03-08 15:12:31 +0100

    ✨ Add filter to function to calculate route

--- app/src/types/routes.tsx
+++ app/src/types/routes.tsx
@@ -2,12 +2,16 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { Position } from 'geojson';
 import { ARParcours } from '../actions/parcours';
 import { POI } from '../actions/poi';
-import { ARPath } from '../actions/routes';
+import { ARPath, RouteProfile } from '../actions/routes';
 
 export type StackParamList = {
   Home: undefined;
   POIDetails: { poi: POI };
-  ChooseItinerary: { start?: Position; end?: Position };
+  ChooseItinerary: {
+    start?: Position;
+    end?: Position;
+    transportMode: RouteProfile;
+  };
   Favorites: undefined;
   Navigation: { path: ARPath };
   RouteDetail: { parcours: ARParcours };
