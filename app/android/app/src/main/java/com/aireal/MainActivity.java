2bb5d1f5a805fce687e61b7195fe98a4b820fd9d
Julien Smolareck <julien.smolareck@lonestone.studio>
2022-03-10 14:48:39 +0100

    💄 Add splashscreen

--- app/android/app/src/main/java/com/aireal/MainActivity.java
+++ app/android/app/src/main/java/com/aireal/MainActivity.java
@@ -1,8 +1,22 @@
 package com.aireal;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState, persistentState);
+
+
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
