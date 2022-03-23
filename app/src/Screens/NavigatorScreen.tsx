e70fea956d44afdc0e377535b156ecec0d1e4654
Julien Smolareck <julien.smolareck@lonestone.studio>
2022-03-23 14:41:58 +0100

    💄 Change Bottom bar

--- app/src/Screens/NavigatorScreen.tsx
+++ app/src/Screens/NavigatorScreen.tsx
@@ -1,9 +1,11 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   createStackNavigator,
-  StackNavigationOptions
+  StackNavigationOptions,
 } from '@react-navigation/stack';
 import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ARCommonHeader from '../components/molecules/ARCommonHeader';
 import ARChooseItinerary from '../components/templates/ARChooseItinerary';
 import ARListFavorites from '../components/templates/ARListFavorites';
@@ -19,33 +21,70 @@ import NavigationScreen from './NavigationScreen';
 import ProfileScreen from './ProfileScreen';
 import RoutesScreen from './RoutesScreen';
 
-const Tab = createMaterialBottomTabNavigator<TabParamList>();
+const Tab = createBottomTabNavigator<TabParamList>();
 const Stack = createStackNavigator<StackParamList>();
 
-const Home = () => (
-  <Tab.Navigator barStyle={{ backgroundColor: theme.colors.primary }}>
-    <Tab.Screen
-      name="Carte"
-      component={MapScreen}
-      options={{ tabBarIcon: 'map' }}
-    />
-    <Tab.Screen
-      name="Itinéraires"
-      options={{ tabBarIcon: 'near-me' }}
-      component={ItineraryScreen}
-    />
-    <Tab.Screen
-      name="Parcours"
-      options={{ tabBarIcon: 'run' }}
-      component={RoutesScreen}
-    />
-    <Tab.Screen
-      name="Profil"
-      options={{ tabBarIcon: 'account-circle' }}
-      component={ProfileScreen}
-    />
-  </Tab.Navigator>
-);
+const routeIcon = {
+  Carte: 'map',
+  Itinéraires: 'near-me',
+  Parcours: 'run',
+  Profil: 'account-circle',
+};
+
+const Home = () => {
+  const { bottom } = useSafeAreaInsets();
+  return (
+    <Tab.Navigator
+      defaultScreenOptions={{
+        headerShown: false,
+      }}
+      screenOptions={({ route, navigation }) => ({
+        tabBarStyle: {
+          backgroundColor: theme.colors.primary,
+          paddingVertical: 12,
+          height: 60 + bottom,
+        },
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.white,
+        tabBarInactiveTintColor: 'rgba(255,255,255, 0.6)',
+
+        tabBarIcon: ({ focused, color, size }) => (
+          <Icon name={routeIcon[route.name]} size={30} color={color} />
+        ),
+
+        // tabBarIni: theme.colors.primary,
+      })}
+      // barStyle={{ backgroundColor: theme.colors.primary }}
+      // screenOptions={({ route }) => ({
+      //   // title: route.name,
+      //   // tabBarIcon: <Icon name={routeIcon[route.name]} size={30} />,
+      // })}
+      // shifting={false}
+    >
+      <Tab.Screen
+        name="Carte"
+        component={MapScreen}
+        // options={{ tabBa: <Icon name={'map'} size={30} /> }}
+        // options={{ tabBarIcon: 'map' }}
+      />
+      <Tab.Screen
+        name="Itinéraires"
+        // options={{ tabBarIcon: 'near-me' }}
+        component={ItineraryScreen}
+      />
+      <Tab.Screen
+        name="Parcours"
+        // options={{ tabBarIcon: 'run' }}
+        component={RoutesScreen}
+      />
+      <Tab.Screen
+        name="Profil"
+        // options={{ tabBarIcon: 'account-circle' }}
+        component={ProfileScreen}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const options: StackNavigationOptions = {
   header: ({ options, navigation }) => (
