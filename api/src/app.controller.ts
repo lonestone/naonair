6871b2ed5ac8f64ade84c500139a88b519167ecf
Johbrun <johan.brun@pm.me>
2022-07-19 16:25:19 +0200

    :wrench: add token checking on BO

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './modules/auth/auth.service';
 import { UniqueTokenGuard } from './modules/auth/guards/unique-token-auth.guard';
 
@@ -10,5 +10,16 @@ export class AppController {
   @UseGuards(UniqueTokenGuard)
   async login() {
     return this.authService.login();
+  }
+
+  @Get('checkToken')
+  async checkToken(@Request() req: Request) {
+    try {
+      const token = req.headers['authorization'].split(' ')[1];
+      await this.authService.checkToken(token);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
