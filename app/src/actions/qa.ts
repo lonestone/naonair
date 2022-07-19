e359d3b6816436974e95fe1d2c82b964a790a805
Johbrun <johan.brun@pm.me>
2022-07-19 16:46:00 +0200

    :bug: fix fav poi QA

--- app/src/actions/qa.ts
+++ app/src/actions/qa.ts
@@ -80,7 +80,7 @@ export const getQAFromBBox = async (bbox: BBox): Promise<QAType> => {
     const { GRAY_INDEX } = json.features[0].properties;
 
     const index = Math.floor(
-      (GRAY_INDEX / 100) * Object.entries(QAValues).length,
+      (GRAY_INDEX / 90) * Object.entries(QAValues).length,
     );
     console.info({ GRAY_INDEX, index });
 
