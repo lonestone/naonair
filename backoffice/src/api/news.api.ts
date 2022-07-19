d19a174bc42036745d2a390c4d55839dd827d722
Johbrun <johan.brun@pm.me>
2022-07-19 18:00:25 +0200

    :sparkles: add button to fold/unfold itineary details

--- backoffice/src/api/news.api.ts
+++ backoffice/src/api/news.api.ts
@@ -4,22 +4,27 @@ import { CreateNewsDTO, NewsDTO, UpdateNewsDTO } from "@aireal/dtos";
 import { AxiosResponse } from "axios";
 
 export const getNews = async () => {
-  const res:AxiosResponse  = await request.get(settings.apiUrl + "/news");
+  const res: AxiosResponse = await request.get(settings.apiUrl + "/news");
   return res.data;
 };
 
 export const getNewsByUUID = async (uuid: string) => {
-  const res:AxiosResponse  = await request.get<NewsDTO>(settings.apiUrl + `/news/${uuid}`);
+  const res: AxiosResponse = await request.get<NewsDTO>(
+    settings.apiUrl + `/news/${uuid}`,
+  );
   return res.data;
 };
 
 export const createNews = async (news: CreateNewsDTO) => {
-  const res:AxiosResponse = await request.post(settings.apiUrl + "/news", news);
+  const res: AxiosResponse = await request.post(
+    settings.apiUrl + "/news",
+    news,
+  );
   return res.data;
 };
 
 export const updateNews = async (news: UpdateNewsDTO) => {
-  const res:AxiosResponse  = await await request.patch(
+  const res: AxiosResponse = await await request.patch(
     settings.apiUrl + "/news/" + news.uuid,
     news,
   );
@@ -27,6 +32,8 @@ export const updateNews = async (news: UpdateNewsDTO) => {
 };
 
 export const removeNews = async (id: string) => {
-  const res:AxiosResponse  = await request.delete(settings.apiUrl + "/news/" + id);
+  const res: AxiosResponse = await request.delete(
+    settings.apiUrl + "/news/" + id,
+  );
   return res.data;
 };
