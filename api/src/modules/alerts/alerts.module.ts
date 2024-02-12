3298c6d242926e3428811c04476a999dd9256bc3
Tatiana Grange <tatiana@lonestone.studio>
2024-02-12 14:08:19 +0100

    🔨 Add cron and add alert saving

--- api/src/modules/alerts/alerts.module.ts
+++ api/src/modules/alerts/alerts.module.ts
@@ -1,12 +1,15 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { AlertsEntity } from 'src/entities/alerts.entity';
+import { FirebaseService } from '../firebase/firebase.service';
 import { AlertsController } from './alerts.controller';
 import { AlertsConverterService } from './alerts.converter';
 import { AlertsService } from './alerts.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, MikroOrmModule.forFeature([AlertsEntity])],
   controllers: [AlertsController],
-  providers: [AlertsService, AlertsConverterService],
+  providers: [AlertsService, AlertsConverterService, FirebaseService],
 })
 export class AlertsModule {}
