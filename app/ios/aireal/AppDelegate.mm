9837adcb0858c3d8459c295fb8eae34492393ed7
Cédric Robert <cedric.robert@lonestone.io>
2025-04-11 15:10:06 +0200

    1.1.13 try

--- app/ios/aireal/AppDelegate.mm
+++ app/ios/aireal/AppDelegate.mm
@@ -1,8 +1,8 @@
 #import "RNFBMessagingModule.h"
 #import "AppDelegate.h"
 #import <Firebase.h>
+#import <FirebaseAnalytics/FIRAnalytics.h>
 #import <React/RCTBundleURLProvider.h>
-
 
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -13,6 +13,8 @@
   self.initialProps = @{};
   self.initialProps = [RNFBMessagingModule addCustomPropsToUserProps:nil withLaunchOptions:launchOptions];
   [FIRApp configure];
+  
+  [FIRAnalytics setAnalyticsCollectionEnabled:YES];
   
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
